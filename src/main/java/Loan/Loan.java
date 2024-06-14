374da284b3e66a52464dbc1727674abbb8bd8670
moises <moiseszandonai1234@gmail.com>
2024-06-13 21:55:48 -0300

    Abstact class Loan

--- src/main/java/Loan/Loan.java
+++ src/main/java/Loan/Loan.java
@@ -1,4 +1,14 @@
 package Loan;
 
-public interface Loan {
+public abstract class Loan {
+
+    protected String customer;
+
+    protected  Double value;
+
+    protected Integer installments;
+
+    protected Double interestRate;
+
+    public abstract Double getTotalLoan();
 }
