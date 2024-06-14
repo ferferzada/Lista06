2e4c6cced4d257b07cf18c0ae6ad36191d947a2a
moises <moiseszandonai1234@gmail.com>
2024-06-13 22:36:28 -0300

    Create LocalLoan

--- src/main/java/Loan/Loan.java
+++ src/main/java/Loan/Loan.java
@@ -4,11 +4,48 @@ public abstract class Loan {
 
     protected String customer;
 
-    protected  Double value;
+    protected final Double value;
 
     protected Integer installments;
 
-    protected Double interestRate;
+    protected final Double instalmentRate;
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public Integer getInstallments() {
+        return installments;
+    }
+
+    public Double getInstalmentRate() {
+        return instalmentRate;
+    }
+
+    public Loan(String customer, Double value, Integer installments, Double instalmentRate) {
+        this.customer = customer;
+        this.value = value;
+        this.installments = installments;
+        this.instalmentRate = instalmentRate;
+    }
 
     public abstract Double getTotalLoan();
+
+
+    public Double valueInstallment(boolean delayed, Integer days){
+        if(delayed){
+            return value * (days *(value * instalmentRate));
+        }
+        return valueInstallment();
+    }
+    public Double valueInstallment(){
+        return value;
+    }
+
+
+    public abstract Double getTotalLoan(boolean delayed, Integer days);
 }
---
+++ src/main/java/Loan/LocalLoan.java
@@ -0,0 +1,32 @@
+package Loan;
+
+public class LocalLoan extends Loan {
+    private Double inflationRate;
+
+    public LocalLoan(String customer, Double value, Integer installments, Double instalmentRate, Double inflationRate) {
+        super(customer, value, installments, instalmentRate);
+        this.inflationRate = inflationRate;
+    }
+
+    public Double valueInstallment(boolean delayed, Integer days) {
+        if (delayed) {
+            return super.valueInstallment(delayed, days) + (value * instalmentRate);
+        }
+        return super.valueInstallment();
+    }
+
+    public Double valueInstallment() {
+        return super.valueInstallment();
+    }
+
+    @Override
+    public Double getTotalLoan() {
+        return getTotalLoan(false, null);
+    }
+
+    @Override
+    public Double getTotalLoan(boolean delayed, Integer days) {
+        return getInstallments() * valueInstallment(delayed, days);
+
+    }
+}
